835bc6796f5de20f1901ddfc35b4a41dc50c02cc
varunkainth <varunkainth22@gmail.com>
2024-06-20 16:39:45 +0530

    Using express

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
-const http = require('http');
+// const http = require('http');
 
-const hostname = '127.0.0.1';
-const port = 8080;
+// const hostname = '127.0.0.1';
+// const port = 8080;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, World!\n');
+// const server = http.createServer((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader('Content-Type', 'text/plain');
+//     res.end('Hello, World!\n');
+// });
+
+// server.listen(port, hostname, () => {
+//     console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+ app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+// send hello world
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
 });
+
+app.get("/login",(req,res)=>{
+    res.send("login page");
+})
