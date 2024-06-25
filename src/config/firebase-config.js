e92b069e3dd2e933c08b98d7d3217836716e75e1
Ragini Mishra <raginimishraanuj@gmail.com>
2024-06-25 18:27:55 +0530

    Add-mongodb connection

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -1,11 +1,10 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import dotenv from 'dotenv';
+dotenv.config();
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
   apiKey: "AIzaSyB3L2yYcFWDlMGxd1F__TU1AubUEHXCbJc",
   authDomain: "lending-buddha-4fa78.firebaseapp.com",
