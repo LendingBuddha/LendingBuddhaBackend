7bf9a093af1d4522cb5c55380d93742db4c85ae2
Ragini Mishra <raginimishraanuj@gmail.com>
2024-07-02 17:54:19 +0530

    Add-chat room server side integration

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
 import express from "express";
 import AuthRoute from "./routes/auth.js";
 import connectDb from "./config/db.js";
+import http from "http";
+import { Server } from "socket.io";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 
 const app = express();
 
-
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+  },
+});
+app.use(cors());
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(express.static("public"));
-app.use(cookieParser()); 
-
+app.use(cookieParser());
 
 await connectDb()
   .then(() => {
     const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => {
+    server.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
 
@@ -24,6 +33,15 @@ await connectDb()
     // send hello world
     app.get("/", (req, res) => {
       res.send("Hello, World!");
+    });
+    io.on("connection", (socket) => {
+      console.log("New client connected!");
+      socket.on("sendMessage", (message) => {
+        io.emit("receiveMessage", message);
+      });
+      socket.on("disconnect", () => {
+        console.log("Client Disconnected!");
+      });
     });
   })
   .catch((error) => {
