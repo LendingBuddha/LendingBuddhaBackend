51a2fc8e3ee1f75d60e85030e7bc99f5025b0499
sunnydowari <86121971+sunnydowari@users.noreply.github.com>
2024-07-12 16:24:42 +0530

    REFACTOR: prod changes

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const io = new Server(server, {
 });
 app.use(
   cors({
-    origin: ["*", "http://localhost:5173"],
+    origin: ["http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
   })
