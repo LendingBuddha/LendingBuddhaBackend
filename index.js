dcb88dd49501c5edb49b066559a2a1543e197810
sunnydowari <86121971+sunnydowari@users.noreply.github.com>
2024-07-12 16:29:53 +0530

    BUILD: production cors bug(explicit)

--- index.js
+++ index.js
@@ -37,6 +37,14 @@ app.use(
     credentials: true,
   })
 );
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+}); 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(express.static("public"));
