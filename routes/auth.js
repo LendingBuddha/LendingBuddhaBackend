cec5737f254070980b73c339eb5f31ef0625c346
sunnydowari <86121971+sunnydowari@users.noreply.github.com>
2024-07-17 18:45:26 +0530

    REFACTOR: added role in login/signup

--- routes/auth.js
+++ routes/auth.js
@@ -238,7 +238,8 @@ router.post("/login/lender", async (req, res) => {
       data: {
         email: user.email,
         uid: user.uid,
-        displayName: user.displayName,
+        displayName: user.fullName,
+        role: "lender"
       },
       refreshToken: refreshToken,
     });
@@ -273,7 +274,8 @@ router.post("/login/borrower", async (req, res) => {
         data: {
           email: user.email,
           uid: user.uid,
-          name: user.displayName,
+          name: user.fullName,
+          role: "borrower",
         },
         refreshToken: refreshToken,
       });
